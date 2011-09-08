f0f6011a97fe66cd3f610835ae972c8e0f78422e
Lucas Cavalcanti <lucas.cavalcanti@caelum.com.br>
2011-09-08 14:18:48 -0300

    testes de aceitacao

---
+++ AcceptanceTests/BaseDosTestesDeAceitacao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Selenium;
+
+namespace AcceptanceTests
+{
+    public class BaseDosTestesDeAceitacao
+    {
+        protected DefaultSelenium browser;
+
+        [SetUp]
+        public void SetUp()
+        {
+            browser = new DefaultSelenium("localhost", 4444, "*firefox", "http://localhost:49278");
+            browser.Start();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            browser.Stop();
+        }
+
+    }
+}
---
+++ AcceptanceTests/EstabelecimentosAccTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Selenium;
+
+namespace AcceptanceTests
+{
+    [TestFixture]
+    public class EstabelecimentosAccTest : BaseDosTestesDeAceitacao
+    {        
+        [Test]
+        public void DeveAdicionarUmEstabelecimento()
+        {
+            browser.Open("/Estabelecimentos");
+            browser.Type("estabelecimento.nome", "Estabelecimento X");
+            browser.Type("estabelecimento.endereco", "Rua Y");
+            browser.Click("adicionar");
+            browser.WaitForPageToLoad("3000");
+
+            Assert.IsTrue(browser.IsTextPresent("Estabelecimento X"));
+            Assert.IsTrue(browser.IsTextPresent("Rua Y"));
+        }
+    }
+}
